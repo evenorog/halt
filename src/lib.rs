09962cc8cf14a7f85e0c6f303d29859366eac911
evenorog <evenorog@gmail.com>
2019-10-17 20:12:17 +0200

    Add Halt iterator

--- src/lib.rs
+++ src/lib.rs
@@ -1,47 +1,172 @@
-use std::sync::{Condvar, Mutex, Weak};
+use std::io::{self, Read, Write};
+use std::sync::{Arc, Condvar, Mutex, Weak};
 
 #[derive(Copy, Clone, Debug, Eq, PartialEq, Ord, PartialOrd)]
 enum State {
-    Running,
     Paused,
+    Running,
+    Stopped,
+}
+
+pub type Result = std::result::Result<(), Error>;
+
+#[derive(Copy, Clone, Debug)]
+pub enum Error {
+    NoReceiver,
+    FailedToLock,
 }
 
 #[derive(Debug)]
 pub struct Controller {
-    halt: Weak<Receiver>,
+    receiver: Weak<Receiver>,
 }
 
 impl Controller {
     #[inline]
-    pub fn pause(&self) {
-        if let Some(halt) = self.halt.upgrade() {
-            let mut guard = halt.state.lock().unwrap();
-            *guard = State::Paused;
+    pub fn pause(&self) -> Result {
+        self.set(State::Paused)
+    }
+
+    #[inline]
+    pub fn resume(&self) -> Result {
+        self.set_and_notify(State::Running)
+    }
+
+    #[inline]
+    pub fn stop(&self) -> Result {
+        self.set_and_notify(State::Stopped)
+    }
+
+    #[inline]
+    fn set(&self, state: State) -> Result {
+        match self.receiver.upgrade() {
+            Some(receiver) => {
+                let mut guard = receiver.state.lock().map_err(|_| Error::FailedToLock)?;
+                *guard = state;
+                Ok(())
+            }
+            None => Err(Error::NoReceiver),
         }
     }
 
     #[inline]
-    pub fn resume(&self) {
-        if let Some(halt) = self.halt.upgrade() {
-            let mut guard = halt.state.lock().unwrap();
-            *guard = State::Running;
-            halt.notify.notify_all();
+    fn set_and_notify(&self, state: State) -> Result {
+        match self.receiver.upgrade() {
+            Some(receiver) => {
+                let mut guard = receiver.state.lock().map_err(|_| Error::FailedToLock)?;
+                *guard = state;
+                receiver.notify.notify_all();
+                Ok(())
+            }
+            None => Err(Error::NoReceiver),
         }
     }
 }
 
 #[derive(Debug)]
-pub struct Receiver {
+struct Receiver {
     state: Mutex<State>,
     notify: Condvar,
 }
 
-impl Receiver {
+#[derive(Debug)]
+pub struct Halt<T> {
+    inner: T,
+    receiver: Arc<Receiver>,
+}
+
+impl<T> Halt<T> {
     #[inline]
-    pub fn wait_if_paused(&self) {
-        let mut guard = self.state.lock().unwrap();
-        while *guard == State::Paused {
-            guard = self.notify.wait(guard).unwrap();
+    pub fn new(inner: impl Into<T>) -> Halt<T> {
+        Halt::from(inner.into())
+    }
+
+    #[inline]
+    pub fn controller(&self) -> Controller {
+        Controller {
+            receiver: Arc::downgrade(&self.receiver),
         }
+    }
+
+    #[inline]
+    fn stopped(&self) -> bool {
+        self.receiver
+            .state
+            .lock()
+            .map(|guard| *guard == State::Stopped)
+            .unwrap_or(false)
+    }
+
+    #[inline]
+    fn wait_if_paused(&self) -> Result {
+        let mut guard = self
+            .receiver
+            .state
+            .lock()
+            .map_err(|_| Error::FailedToLock)?;
+        while *guard == State::Paused {
+            guard = self
+                .receiver
+                .notify
+                .wait(guard)
+                .map_err(|_| Error::FailedToLock)?;
+        }
+        Ok(())
+    }
+}
+
+impl<T> From<T> for Halt<T> {
+    #[inline]
+    fn from(inner: T) -> Self {
+        Halt {
+            inner,
+            receiver: Arc::new(Receiver {
+                state: Mutex::new(State::Running),
+                notify: Condvar::new(),
+            }),
+        }
+    }
+}
+
+impl<I: Iterator> Iterator for Halt<I> {
+    type Item = I::Item;
+
+    #[inline]
+    fn next(&mut self) -> Option<Self::Item> {
+        let _ = self.wait_if_paused();
+        if self.stopped() {
+            None
+        } else {
+            self.inner.next()
+        }
+    }
+}
+
+impl<R: Read> Read for Halt<R> {
+    #[inline]
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        let _ = self.wait_if_paused();
+        if self.stopped() {
+            Ok(0)
+        } else {
+            self.inner.read(buf)
+        }
+    }
+}
+
+impl<W: Write> Write for Halt<W> {
+    #[inline]
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        let _ = self.wait_if_paused();
+        if self.stopped() {
+            Ok(0)
+        } else {
+            self.inner.write(buf)
+        }
+    }
+
+    #[inline]
+    fn flush(&mut self) -> io::Result<()> {
+        self.inner.flush()
     }
 }
