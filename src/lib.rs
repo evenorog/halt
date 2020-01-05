ae4167ae669dd2a79ba1a1cf587c42b5f1b4ce92
evenorog <evenorog@gmail.com>
2020-01-05 22:07:26 +0100

    Use default for Arc

--- src/lib.rs
+++ src/lib.rs
@@ -32,6 +32,141 @@ use std::io::{self, Read, Write};
 use std::sync::{Arc, Condvar, Mutex, Weak};
 use Status::{Paused, Running, Stopped};
 
+/// Returns a new wrapper around `T`.
+///
+/// # Examples
+/// ```
+/// halt::new(0..10);
+/// ```
+pub fn new<T>(inner: T) -> Halt<T> {
+    Halt::from(inner)
+}
+
+/// A wrapper that makes it possible to pause, stop, and resume iterators, readers, and writers.
+///
+/// # Examples
+/// ```
+/// for i in halt::new(0..10) {
+///     dbg!(i);
+/// }
+/// ```
+#[derive(Debug, Default)]
+pub struct Halt<T> {
+    inner: T,
+    state: Arc<State>,
+}
+
+impl<T> Halt<T> {
+    /// Returns a remote that allows for pausing, stopping, and resuming the `T`.
+    pub fn remote(&self) -> Remote {
+        Remote {
+            state: Arc::downgrade(&self.state),
+        }
+    }
+
+    /// Returns `true` if running.
+    pub fn is_running(&self) -> bool {
+        self.state.is_running()
+    }
+
+    /// Returns `true` if paused.
+    pub fn is_paused(&self) -> bool {
+        self.state.is_paused()
+    }
+
+    /// Returns `true` if stopped.
+    pub fn is_stopped(&self) -> bool {
+        self.state.is_stopped()
+    }
+
+    /// Returns a reference to the inner `T`.
+    pub fn get_ref(&self) -> &T {
+        &self.inner
+    }
+
+    /// Returns a mutable reference to the inner `T`.
+    pub fn get_mut(&mut self) -> &mut T {
+        &mut self.inner
+    }
+
+    /// Returns the inner `T`.
+    pub fn into_inner(self) -> T {
+        self.inner
+    }
+
+    fn wait_if_paused(&self) {
+        let mut guard = self.state.status.lock().unwrap();
+        while *guard == Paused {
+            guard = self.state.condvar.wait(guard).unwrap();
+        }
+    }
+}
+
+impl<T> From<T> for Halt<T> {
+    fn from(inner: T) -> Self {
+        Halt {
+            inner,
+            state: Arc::default(),
+        }
+    }
+}
+
+impl<I: Iterator> Iterator for Halt<I> {
+    type Item = I::Item;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.wait_if_paused();
+        if self.is_stopped() {
+            None
+        } else {
+            self.inner.next()
+        }
+    }
+}
+
+impl<I: DoubleEndedIterator> DoubleEndedIterator for Halt<I> {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        self.wait_if_paused();
+        if self.is_stopped() {
+            None
+        } else {
+            self.inner.next_back()
+        }
+    }
+}
+
+impl<A, I: Extend<A>> Extend<A> for Halt<I> {
+    fn extend<T: IntoIterator<Item = A>>(&mut self, iter: T) {
+        self.inner.extend(iter);
+    }
+}
+
+impl<R: Read> Read for Halt<R> {
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        self.wait_if_paused();
+        if self.is_stopped() {
+            Ok(0)
+        } else {
+            self.inner.read(buf)
+        }
+    }
+}
+
+impl<W: Write> Write for Halt<W> {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        self.wait_if_paused();
+        if self.is_stopped() {
+            Ok(0)
+        } else {
+            self.inner.write(buf)
+        }
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        self.inner.flush()
+    }
+}
+
 #[derive(Copy, Clone, Debug, Hash, Eq, PartialEq, Ord, PartialOrd)]
 enum Status {
     Running,
@@ -153,140 +288,5 @@ impl Remote {
         if need_to_notify {
             state.condvar.notify_all();
         }
-    }
-}
-
-/// Returns a new wrapper around `T`.
-///
-/// # Examples
-/// ```
-/// halt::new(0..10);
-/// ```
-pub fn new<T>(inner: T) -> Halt<T> {
-    Halt::from(inner)
-}
-
-/// A wrapper that makes it possible to pause, stop, and resume iterators, readers, and writers.
-///
-/// # Examples
-/// ```
-/// for i in halt::new(0..10) {
-///     dbg!(i);
-/// }
-/// ```
-#[derive(Debug, Default)]
-pub struct Halt<T> {
-    inner: T,
-    state: Arc<State>,
-}
-
-impl<T> Halt<T> {
-    /// Returns a remote that allows for pausing, stopping, and resuming the `T`.
-    pub fn remote(&self) -> Remote {
-        Remote {
-            state: Arc::downgrade(&self.state),
-        }
-    }
-
-    /// Returns `true` if running.
-    pub fn is_running(&self) -> bool {
-        self.state.is_running()
-    }
-
-    /// Returns `true` if paused.
-    pub fn is_paused(&self) -> bool {
-        self.state.is_paused()
-    }
-
-    /// Returns `true` if stopped.
-    pub fn is_stopped(&self) -> bool {
-        self.state.is_stopped()
-    }
-
-    /// Returns a reference to the inner `T`.
-    pub fn get_ref(&self) -> &T {
-        &self.inner
-    }
-
-    /// Returns a mutable reference to the inner `T`.
-    pub fn get_mut(&mut self) -> &mut T {
-        &mut self.inner
-    }
-
-    /// Returns the inner `T`.
-    pub fn into_inner(self) -> T {
-        self.inner
-    }
-
-    fn wait_if_paused(&self) {
-        let mut guard = self.state.status.lock().unwrap();
-        while *guard == Paused {
-            guard = self.state.condvar.wait(guard).unwrap();
-        }
-    }
-}
-
-impl<T> From<T> for Halt<T> {
-    fn from(inner: T) -> Self {
-        Halt {
-            inner,
-            state: Arc::new(State::default()),
-        }
-    }
-}
-
-impl<I: Iterator> Iterator for Halt<I> {
-    type Item = I::Item;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        self.wait_if_paused();
-        if self.is_stopped() {
-            None
-        } else {
-            self.inner.next()
-        }
-    }
-}
-
-impl<I: DoubleEndedIterator> DoubleEndedIterator for Halt<I> {
-    fn next_back(&mut self) -> Option<Self::Item> {
-        self.wait_if_paused();
-        if self.is_stopped() {
-            None
-        } else {
-            self.inner.next_back()
-        }
-    }
-}
-
-impl<A, I: Extend<A>> Extend<A> for Halt<I> {
-    fn extend<T: IntoIterator<Item = A>>(&mut self, iter: T) {
-        self.inner.extend(iter);
-    }
-}
-
-impl<R: Read> Read for Halt<R> {
-    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
-        self.wait_if_paused();
-        if self.is_stopped() {
-            Ok(0)
-        } else {
-            self.inner.read(buf)
-        }
-    }
-}
-
-impl<W: Write> Write for Halt<W> {
-    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
-        self.wait_if_paused();
-        if self.is_stopped() {
-            Ok(0)
-        } else {
-            self.inner.write(buf)
-        }
-    }
-
-    fn flush(&mut self) -> io::Result<()> {
-        self.inner.flush()
     }
 }
